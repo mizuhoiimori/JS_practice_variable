375f43aee830b902f69421a9861895953cc7b8e3
iimorimizuho <tanaka.mizuho.com@gmail.com>
2021-11-01 15:49:04 +0900

    first commit

---
+++ sample.js
@@ -0,0 +1,32 @@
+// const nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
+
+// var str = "webcamp"
+
+// function foo(){
+//   let x = "webcamp"
+//   {
+//     let y = "hello webcamp"
+//   }
+//   console.log(x)
+//   console.log(y)
+// }
+
+// foo()
+
+// for(let i = 0; i < 10; i++){
+//   console.log(i)
+// }
+// console.log(i)
+
+var str = "webcamp"
+
+function foo(){
+  console.log(str)
+  var str = "dmm webcamp"
+  console.log(str)
+}
+
+foo()
